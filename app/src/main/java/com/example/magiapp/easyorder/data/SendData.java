690b8f78cb4e3bcc4e6a6dcd1ef4427271990fce
Coregame <thewegang@gmail.com>
2017-11-07 21:55:42 +0700

    (Unfinished) Send POST JSON to server

--- app/src/main/java/com/example/magiapp/easyorder/data/SendData.java
+++ app/src/main/java/com/example/magiapp/easyorder/data/SendData.java
@@ -6,6 +6,17 @@ import android.util.Log;
 import com.example.magiapp.easyorder.ConfirmOrderActivity;
 import com.example.magiapp.easyorder.R;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -25,14 +36,37 @@ public class SendData{
     }
 
     public boolean send() {
+
         /*
-        Send a data to server (Por's job)
-
+        @todo: Send a data to server (Por's job)
         Implement your code here
-
         Maximum time = 6000ms otherwise this function would be killed.
          */
+
         try {
+            //Connect
+            String data = "{ \"tableNum\": 78}";
+            //String url = "http://localhost:8080/order";
+
+            URL url_server = new URL("http://localhost:8080/order");
+            HttpURLConnection httpConn = (HttpURLConnection) url_server.openConnection();
+            //HttpURLConnection httpConn = (HttpURLConnection) ((new URL (url).openConnection()));
+
+            httpConn.setDoOutput(true);
+            httpConn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+            httpConn.setRequestProperty("Accept", "application/json");
+            httpConn.setRequestMethod("POST");
+
+            //httpConn.setDoInput(true);
+            httpConn.connect();
+
+            //Write
+            OutputStream os = httpConn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(data);
+            writer.close();
+            os.close();
+
             Log.d("Send data", "Send to "+ip +"\t Table no." + tableNum);
             Log.d("Send data", foodItemsList.toString());
             Thread.sleep(2000);
@@ -40,8 +74,14 @@ public class SendData{
             Log.d("Thread", "true");
             return true;
 
-        }catch (InterruptedException e){
-
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return false;
